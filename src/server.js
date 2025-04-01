45a42e7e02d5c67333880e779d4aef25979a31af
aryanmishra333 <aryangungun4187@gmail.com>
2025-04-01 16:55:16 -0400

    Docker Containerized

--- src/server.js
+++ src/server.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
 // API routes for functions
 app.use('/api', functionRoutes);
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on port 3000');
 });
